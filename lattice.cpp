ec089324f3fff6b489ced982f5c344cbd4a02363
Simon Lizotte <simon.lizotte.1@ulaval.ca>
2020-11-07 14:34:12 -0500

    fix neighbours with parity

--- lattice.cpp
+++ lattice.cpp
@@ -41,9 +41,10 @@ void Lattice::setDensityOutsideSnowflake() {
 
 
 list<pair<long int, long int>> Lattice::getNeighboursIndicesOf(long int i, long int j) const{
+    int parity = i % 2;
 
     list<pair<long int, long int>> neighbours {
-        {i-1, j}, {i-1, j+1}, {i, j-1}, {i, j+1}, {i+1, j}, {i+1, j+1}
+        {i, j}, {i-1, j+parity}, {i-1, j+1+parity}, {i, j-1}, {i, j+1}, {i+1, j+parity}, {i+1, j+1+parity}
     };
 
     filterInvalidIndices(neighbours);
