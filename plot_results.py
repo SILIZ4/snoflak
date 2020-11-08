dfc76c614b83d41e2297676e0cea58fdebc358d2
Philippe-André Luneau <paluneau@videotron.ca>
2020-11-07 23:38:25 -0500

    idk plot plot plot

--- plot_results.py
+++ plot_results.py
@@ -3,11 +3,11 @@ from matplotlib import pyplot
 from hex_grid import hex_grid
 
 
-with open("snowflake_1500_vapor", "rb") as file_stream:
+with open("frames/snowflake_2300_vapor", "rb") as file_stream:
     data1 = numpy.fromfile(file_stream, dtype=numpy.double).reshape(500, 500)
-with open("snowflake_1500_solid", "rb") as file_stream:
+with open("frames/snowflake_2300_solid", "rb") as file_stream:
     data2 = numpy.fromfile(file_stream, dtype=numpy.double).reshape(500, 500)
-with open("snowflake_1500_liquid", "rb") as file_stream:
+with open("frames/snowflake_2300_liquid", "rb") as file_stream:
     data3 = numpy.fromfile(file_stream, dtype=numpy.double).reshape(500, 500)
 
 hex_grid(data1+data2+data3, cmap="Blues")
