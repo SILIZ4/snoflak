5c7e4198a1ab8fb5240de784a324ccd21ee6bfd0
Philippe-André Luneau <paluneau@videotron.ca>
2020-11-07 21:12:21 -0500

    Colourmap fixe pour les gifs

--- plot_results_gif.py
+++ plot_results_gif.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 from hex_grid import hex_grid
 import PIL.Image
@@ -15,11 +15,22 @@ import os
 
 
 def view_snoflak_gif(number_iter):
+    dataframes = []
     frame = []
-    for i in range(0, number_iter) :
-        with open("frames10000/snowflake_" + str(i) + "_solid", "rb") as file_stream:
-            data = numpy.fromfile(file_stream, dtype=numpy.double).reshape(100, 100)
-        plt.imshow(data.T)
+    max_val = -float("inf")
+    for i in range(0, number_iter-1) :
+        with open("frames/snowflake_" + str(i) + "_solid", "rb") as file_stream:
+            data = np.fromfile(file_stream, dtype=np.double).reshape(100, 100)
+            dataframes.append(data)
+            tmp_max = np.max(data)
+            if(tmp_max > max_val):
+                max_val = tmp_max
+
+
+    for i in range(0, number_iter-1) :
+        #with open("frames/snowflake_" + str(i) + "_solid", "rb") as file_stream:
+        #   data = numpy.fromfile(file_stream, dtype=numpy.double).reshape(100, 100)
+        plt.imshow(dataframes[i].T, cmap="Blues", vmin=0, vmax=max_val)
         plt.colorbar()
         plt.savefig(str(i+1))
         plt.clf()
@@ -27,7 +38,7 @@ def view_snoflak_gif(number_iter):
         frame.append(PIL.Image.open(str(i+1) + ".png"))
         os.remove(str(i+1) + ".png")
 
-    frame[0].save("gifs/snoflak10000.gif", save_all=True, append_images=frame[1:], optimize=False, duration=100, loop=0)
+    frame[0].save("gifs/snoflak_theta_alpha.gif", save_all=True, append_images=frame[1:], optimize=False, duration=100, loop=0)
 
 
-view_snoflak_gif(913)
+view_snoflak_gif(99)
