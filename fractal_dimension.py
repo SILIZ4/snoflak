e1660a0d43089eb284619626b2a05618736f1e73
Philippe-André Luneau <paluneau@videotron.ca>
2020-11-08 03:42:39 -0500

    amélioration de la dimension fractale

--- fractal_dimension.py
+++ fractal_dimension.py
@@ -4,33 +4,39 @@ from matplotlib import pyplot
 from hex_grid import hex_grid
 
 
+# Since liquid mass is present almost everywhere along the boundary,
+# it's a good approximation of it, so we can use the liquid mass map
+# to compute fractal dimension of the snowflake's boundary
 with open("test_fractal/snowflake_5000_liquid", "rb") as file_stream:
-    data3 = np.fromfile(file_stream, dtype=np.double).reshape(500, 500)
-hex_grid(data3, cmap="Greys")
-# Box-counting dimension: lim eps->0 [log N(eps) / log(1/eps)]
+    data = np.fromfile(file_stream, dtype=np.double).reshape(500, 500)
+data[data>0]=1
 
-# dividers of 500
+
+# dividers of 500 (if the frame is n x n, choose dividers of n)
 divisions = [5,10,20,25,50,100,250,500]
 EPS = np.zeros((len(divisions),1))
 NEPS = np.zeros((len(divisions),1))
 
 
+# covering of the boundary with div x div boxes
 for k in range(0,len(divisions)):
     i = divisions[k]
-    dx = mt.floor(500/i)
-    doesCover = np.zeros((i*i,1))
+    eps = mt.floor(500 / i)
+    isCovering = np.zeros((i * i, 1))
     for j in range(0,i*i):
-        xi_1 = (dx*j)%500
-        xi_2 = (dx*j)%500 + dx
-        yi_1 = dx*mt.floor(float(j)/float(i))
-        yi_2 = dx*mt.floor(float(j)/float(i)) + dx
-        for a in range(xi_1,xi_2):
-            for b in range(yi_1,yi_2):
-                if(data3[a,b]>0):
-                    doesCover[j] = 1
-    NEPS[k] =  np.sum(doesCover)
-    EPS[k] = dx
+        x_1 = (eps * j) % 500
+        x_2 = (eps * j) % 500 + eps
+        y_1 = eps * mt.floor(float(j) / float(i))
+        y_2 = eps * mt.floor(float(j) / float(i)) + eps
+        for a in range(x_1,x_2):
+            for b in range(y_1,y_2):
+                if(data[a,b] == 1):
+                    isCovering[j] = 1
+    NEPS[k] = np.sum(isCovering)
+    EPS[k] = eps
 
+# Box-counting dimension: lim eps->0 [log N(eps) / log(1/eps)]
 dim, _ = np.polyfit(np.log(np.reciprocal(EPS)).flatten(),np.log(NEPS).flatten(),1)
 
 print(dim)
+hex_grid(data, cmap="binary")
--- plot_results.py
+++ plot_results.py
@@ -3,14 +3,12 @@ from matplotlib import pyplot
 from hex_grid import hex_grid
 
 
-with open("frames/snowflake_2300_vapor", "rb") as file_stream:
-    data1 = numpy.fromfile(file_stream, dtype=numpy.double).reshape(500, 500)
-with open("frames/snowflake_2300_solid", "rb") as file_stream:
-    data2 = numpy.fromfile(file_stream, dtype=numpy.double).reshape(500, 500)
-with open("frames/snowflake_2300_liquid", "rb") as file_stream:
+with open("snowflake_5000_vapor", "rb") as file_stream:
+    data1 = numpy.fromfile(file_stream, dtype=numpy.double).reshape(600, 600)
+with open("snowflake_5000_solid", "rb") as file_stream:
+    data2 = numpy.fromfile(file_stream, dtype=numpy.double).reshape(600, 600)
+with open("snowflake_5000_liquid", "rb") as file_stream:
     data3 = numpy.fromfile(file_stream, dtype=numpy.double).reshape(500, 500)
 
-hex_grid(data1+data2+data3, cmap="Blues")
-#pyplot.imshow(data, cmap="Blues")
-#pyplot.colorbar()
-#pyplot.show()
+hex_grid(data3, cmap="Blues")
+
